520be9ffd9c0edbcfed87e7522c16ed6d054dee4
Steven Ferrell <ferr.mail@gmail.com>
2018-06-27 18:31:32 -0400

    Added yargs

--- bin/derp.js
+++ bin/derp.js
@@ -1,41 +1,10 @@
 #!/usr/bin/env node
 
 var derp = require('../lib/index');
+let yargs = require('yargs');
 
-// Delete the 0 and 1 argument (node and script.js)
-var args = process.argv.splice(process.execArgv.length + 2);
+var argv = require('yargs').argv;
 
-if (args.length > 0) {
-    var arg1 = args[0];
-
-    switch (arg1) {
-        case '--version':
-            {
-                derp.displayVersion();
-                break;
-            }
-        case 'start':
-            {
-                derp.start();
-                break;
-            }
-        case 'deploy':
-            {
-                if (args[1]) {
-                    let endpointName = args[1];
-                    derp.deploy(endpointName);
-                } else {
-                    console.log('Deploy command requires an endpoint name');
-                }
-                break;
-            }
-        default:
-            {
-                console.log(`Unrecognized argument ${arg1}`);
-                break;
-            }
-    }
-} else {
-    //display help commands
-    console.log('Use --help to see a list of derp commands')
+if (argv.v) {
+    derp.displayVersion();
 }
