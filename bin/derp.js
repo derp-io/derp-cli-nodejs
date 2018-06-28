51df19f1224a1cdbb936cb3e4560ebbe6d2c752d
Steven Ferrell <ferr.mail@gmail.com>
2018-06-28 19:39:27 -0400

    Added yargs commands, yargs is cool

--- bin/derp.js
+++ bin/derp.js
@@ -1,10 +1,28 @@
 #!/usr/bin/env node
 
 var derp = require('../lib/index');
-let yargs = require('yargs');
+const yargs = require('yargs');
 
-var argv = require('yargs').argv;
+yargs
+    .command(['new <endpointName> <httpMethod>'], 'Create a new endpoint', (yargs) => {
+        return yargs.positional('endpointName', {
+                describe: 'Name used for your routed endpoint',
+                type: 'string'
+            }).positional('httpMethod', {
+                describe: 'Type of HTTP Method for this endpoint: GET|POST|PUT|DELETE',
+                type: 'string'
+            })
+            .option('provider', {
+                alias: 'p',
+                default: 'aws-lambda'
+            })
 
-if (argv.v) {
-    derp.displayVersion();
-}
+    }, (argv) => {
+
+        derp.new(`${argv.httpMethod}:${argv.endpointName}`);
+    })
+    .command(['cancel'], 'the cancel command', () => {}, (argv) => {
+        console.log('ok i cancelled it')
+    })
+    .help()
+    .argv
